d257e5778172f16e88c7cd9cf1974e4be17e917c
ZengjfOS <zengjf42@163.com>
2020-03-27 20:30:45 +0800

    support auto generate index list

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,16 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { EOF } from 'dns';
+import * as fs from 'fs';
 
 let MDP_UP = -1;
 let MDP_DOWN = 1;
 
-let cmds = ["list", "salt"];
+let cmds = ["list", "salt", "index"];
+let SALT_BOUNDARY = ["@startsalt", "@endsalt"]
+let INDEX_BOUNDARY = ["", ""]
 
-function findBoundary(editor: vscode.TextEditor, index: number, direction: number) {
+function findBoundary(editor: vscode.TextEditor, index: number, direction: number, boundary:string[]) {
 	let line =index;
 
 	while ((line != -1) && (line != editor.document.lineCount)) {
@@ -16,7 +19,25 @@ function findBoundary(editor: vscode.TextEditor, index: number, direction: numbe
 		let range = new vscode.Range(editorLine.range.start, editorLine.range.end)
 		let lineText = editor.document.getText(range);
 
-		if (lineText.startsWith("@startsalt") || lineText.startsWith("@endsalt")) {
+		if (lineText.startsWith(boundary[0]) || lineText.startsWith(boundary[1])) {
+			return line;
+		}
+
+		line += direction;
+	}
+
+	return -1;
+}
+
+function findEmptyLine(editor: vscode.TextEditor, index: number, direction: number) {
+	let line =index;
+
+	while ((line != -1) && (line != editor.document.lineCount)) {
+	    let editorLine = editor.document.lineAt(line);
+		let range = new vscode.Range(editorLine.range.start, editorLine.range.end)
+		let lineText = editor.document.getText(range);
+
+		if (lineText.length == 0) {
 			return line;
 		}
 
@@ -30,8 +51,8 @@ function doSalt(activeEditor: vscode.TextEditor)
 {
 	var line = activeEditor.selection.active.line;
 
-	let startLine = findBoundary(activeEditor, line, MDP_UP);
-	let endLine = findBoundary(activeEditor, line, MDP_DOWN);
+	let startLine = findBoundary(activeEditor, line, MDP_UP, SALT_BOUNDARY);
+	let endLine = findBoundary(activeEditor, line, MDP_DOWN, SALT_BOUNDARY);
 
 	vscode.window.showInformationMessage("start: " + startLine + ", end: " + endLine);
 
@@ -96,6 +117,86 @@ function doList(activeEditor: vscode.TextEditor)
 	}
 }
 
+function doIndex(activeEditor: vscode.TextEditor)
+{
+	var line = activeEditor.selection.active.line;
+
+	var inputString = vscode.window.showInputBox(
+		{ // 这个对象中所有参数都是可选参数
+			password:false,               // 输入内容是否是密码
+			ignoreFocusOut:true,          // 默认false，设置为true时鼠标点击别的地方输入框不会消失
+			placeHolder:'input relative direcotry：',    // 在输入框内的提示信息
+			prompt:'docs',                // 在输入框下方的提示信息
+			validateInput:function(text){ // 校验输入信息
+				cmds.forEach(element => {
+					if (text.trim() == element)
+						return "";
+					
+				});
+
+				return null;
+			}
+		}).then( msg => {
+			var editor = vscode.window.activeTextEditor;
+			if (editor != undefined) {
+
+				let startLine = findEmptyLine(activeEditor, line, MDP_UP);
+				let endLine = findEmptyLine(activeEditor, line, MDP_DOWN);
+
+				if (startLine == -1) 
+					startLine =  0;
+				else if ((startLine + 1) == activeEditor.document.lineCount)
+					startLine = startLine;
+				else 
+					startLine += 1;
+
+				if (endLine == -1) {
+					if (activeEditor.document.lineCount > 1)
+						endLine = activeEditor.document.lineCount - 1; 
+					else 
+						endLine = 0;
+				}
+
+				if (editor != undefined) {
+					editor.edit(edit => {
+						let range = new vscode.Range(activeEditor.document.lineAt(startLine).range.start, activeEditor.document.lineAt(endLine).range.end)
+						edit.delete(range);
+					}).then((value) => {
+
+						line = startLine;
+
+						if (editor != undefined) {
+							editor.edit(edit => {
+								let folderPath = vscode.workspace.rootPath + "\\" + msg;
+								console.log(folderPath);
+								if (fs.existsSync(folderPath)) {
+									let files = fs.readdirSync(folderPath || "");
+									var outputString = "";
+									files.forEach((file: fs.PathLike) => {
+										const r = /^\d{1,4}_.*\.md/g;
+										const m = r.exec(file.toString());
+										m?.forEach((value, index) => {
+											outputString += "* [" + file + "](" + msg + "/" + file + ")\n";
+										});
+									});
+
+									edit.insert(new vscode.Position(line, 0), outputString);
+									console.log(outputString);
+
+									vscode.window.showInformationMessage("list files over. start: " + startLine + ", end: " + endLine);
+								} else {
+									vscode.window.showInformationMessage("folder Path: " + folderPath + " not exist");
+								}
+							});
+						}
+
+					});
+				}
+			}
+		}
+	);
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -116,11 +217,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 		var inputString = vscode.window.showInputBox(
         	{ // 这个对象中所有参数都是可选参数
-                password:false, 				// 输入内容是否是密码
-                ignoreFocusOut:true, 			// 默认false，设置为true时鼠标点击别的地方输入框不会消失
-                placeHolder:'input cmd：', 		// 在输入框内的提示信息
-				prompt:'salt/list', 			// 在输入框下方的提示信息
-				validateInput:function(text){	// 校验输入信息
+                password:false,               // 输入内容是否是密码
+                ignoreFocusOut:true,          // 默认false，设置为true时鼠标点击别的地方输入框不会消失
+                placeHolder:'input cmd：',    // 在输入框内的提示信息
+				prompt:'salt/list/index',     // 在输入框下方的提示信息
+				validateInput:function(text){ // 校验输入信息
 					cmds.forEach(element => {
 						if (text.trim() == element)
 							return "";
@@ -143,6 +244,8 @@ export function activate(context: vscode.ExtensionContext) {
 							doSalt(activeEditor);
 						} else if (msg.toLowerCase() == "list") {
 							doList(activeEditor);
+						} else if (msg.toLowerCase() == "index") {
+							doIndex(activeEditor);
 						}
 					}
 				}
@@ -168,6 +271,16 @@ export function activate(context: vscode.ExtensionContext) {
 		const activeEditor = vscode.window.activeTextEditor;
 		if (activeEditor) {
 			doList(activeEditor);
+		}
+	});
+
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('extension.mdindex', () => {
+
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor) {
+			doIndex(activeEditor);
 		}
 	});
 
