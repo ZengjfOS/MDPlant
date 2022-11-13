6eba2c2884765a2a7d7a8645386bcf2955193ada
zengjf <zengjf42@163.com>
2022-11-13 15:07:23 +0800

    plugin unit test env

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,24 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import { after, before, it } from 'mocha';
 // import * as myExtension from '../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
+	before(() => {
+		console.log("before")
+	})
+
+	after(() => {
+		console.log('after');
+	});
+
 	test('Sample test', () => {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
 		assert.equal(-1, [1, 2, 3].indexOf(0));
+
+		console.log(vscode.workspace.workspaceFolders)
 	});
 });
